531149f8bf2e4e85f9cd863ae3a541d80eedd4d0
Jubair <mdnahiyan4000@gmail.com>
2024-03-26 15:37:30 +0600

    update

--- what_is_data_analysis.py
+++ what_is_data_analysis.py
@@ -1,4 +1,4 @@
-'''
+"""
 Data Analysis: 
     Data analysis is a process of inspecting, cleansing,
     transforming, and modelling data with the goal of discovering
@@ -7,4 +7,20 @@ Data Analysis:
     Data analysis is also referred to as data mining.
     Data analysis is also referred to as exploratory data analysis.
 
-'''
+1.The name "pandas" has a reference to both "panel data", and "python
+2.data analysis" and was created by wes mckinney in 2008.
+3.Pandas is a python library used for working with data sets.
+4.It has functions
+5.data.
+6.JSON,ZIP etc.
+7.for analyzing, cleaning, exploring, and manipulating
+8.Read and write data structures and different formats : csv, txt, html
+
+Three Data Structures:
+    Series: One-Dimensional labeled arrays pd.Series(data)
+
+    DataFrames: Two-Dimensional data structure with columns, much like a
+    table.
+
+    Panel : A panel is a 3D container of data.
+"""

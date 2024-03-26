ea9add22b8a1f70fb32a1bf4c2b880e4768f99d6
Jubair <mdnahiyan4000@gmail.com>
2024-03-26 15:21:38 +0600

    first commit

---
+++ what_is_data_analysis.py
@@ -0,0 +1,10 @@
+'''
+Data Analysis: 
+    Data analysis is a process of inspecting, cleansing,
+    transforming, and modelling data with the goal of discovering
+    useful information, informing conclusions, and supporting
+    decision-making.
+    Data analysis is also referred to as data mining.
+    Data analysis is also referred to as exploratory data analysis.
+
+'''

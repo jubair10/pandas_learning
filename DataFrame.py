58b74cf39e05c80e2e128422bceef012e3c4e4c7
Jubair <mdnahiyan4000@gmail.com>
2024-03-26 16:29:38 +0600

    update

--- DataFrame.py
+++ DataFrame.py
@@ -1,5 +1,13 @@
 """
 Difference between Dataframe and Series
+
+A Series is a one-dimensional labeled array of data.
+
+A DataFrame is a two-dimensional labeled array of data.
+
+A Panel is a three-dimensional labeled array of data.
+
+
             DataFrame
     Name  Age         City
 0   John   30     New York
@@ -20,6 +28,8 @@ data = {
     "Age": [30, 25, 35],
     "City": ["New York", "Los Angeles", "Chicago"],
 }
+# all value's length must be same
+
 
 # Create a DataFrame from the dictionary
 df = pd.DataFrame(data)
